612226a101ab0dab2fbdf8fb07b1d2c9832dcf5f
Sumit K <synergy2411@outlook.com>
2022-03-12 12:03:35 +0530

    login logout added

--- my-app/src/app/services/auth.service.ts
+++ my-app/src/app/services/auth.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private isAuthenticated : boolean = false;
+
+  constructor(private router :Router) { }
 
   onUserLogin(email : string, password : string){
     // Make the call to server for User verification
     return new Promise((resolve, reject) => {
-      if(email === 'test@test.com' && password === 'test!123#'){
-        resolve({message : "SUCCESS"})
-      }else{
-        reject({error : "NOT Authenticated"})
-      }
+      setTimeout(() => {
+        if(email === 'test@test.com' && password === 'test!123#'){
+          this.isAuthenticated = true
+          resolve({message : "SUCCESS"})
+        }else{
+          reject({error : "NOT Authenticated"})
+        }
+      }, 1500)
     })
   }
+
+  isUserAuthenticated(){
+    return this.isAuthenticated;
+  }
+
+  onUserLogout(){
+    this.isAuthenticated = false;
+    this.router.navigate(['login'])
+  }
+
 }
